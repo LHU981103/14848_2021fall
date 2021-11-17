3c425539b368f689a66750de9d408f24a99b53d7
Lan Hu <lhu98jes@gmail.com>
2021-11-17 17:37:01 -0500

    hw4 results

--- hw4/temperature_mapper.py
+++ hw4/temperature_mapper.py
@@ -3,8 +3,9 @@ import sys
 for line in sys.stdin:
     line = line.strip()
     temperature = int(line[87:92])
-    quality = int(line[93])
+    quality = line[92]
 
     if ((temperature != 9999) and 
-        (quality == 0 or quality == 1 or quality == 4 or quality == 5 or quality == 9)):
+        (quality == "0" or quality == "1" or quality == "4" 
+         or quality == "5" or quality == "9")):
         print('%s\t%d' % (line[15:23], temperature))
